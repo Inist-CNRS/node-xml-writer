ccb3025a8b3f9d3dcd1f5b742e3206487e684bb7
Nicolas Thouvenin <nthouvenin@gmail.com>
2011-11-21 17:23:36 +0100

    ...

---
+++ test/pi.js
@@ -0,0 +1,39 @@
+
+var XMLWriter = require('../');
+exports['setUp'] = function (callback) {
+	this.xw = new XMLWriter;
+	callback();
+};
+exports['t01'] = function (test) {
+	this.xw.startPI('php');
+	this.xw.endPI();
+	test.equal(this.xw.toString(), '<?php?>');
+	test.done();
+
+};
+exports['t02'] = function (test) {
+	this.xw.startPI('php');
+	this.xw.text(' echo');
+	this.xw.text(' __FILE__; ');
+	this.xw.endPI();
+	test.equal(this.xw.toString(), '<?php echo __FILE__; ?>');
+    test.done();
+};
+exports['t03'] = function (test) {
+	this.xw.startPI('xml-stylesheet');
+	this.xw.startAttribute('type');
+	this.xw.text('text/xml');
+	this.xw.endAttribute();
+	this.xw.startAttribute('href');
+	this.xw.text('style.xsl');
+	this.xw.endAttribute();
+	this.xw.endPI();
+	test.equal(this.xw.toString(), '<?xml-stylesheet type="text/xml" href="style.xsl"?>');
+	test.done();
+};
+exports['t04'] = function (test) {
+    test.done();
+};
+exports['t05'] = function (test) {
+    test.done();
+};
